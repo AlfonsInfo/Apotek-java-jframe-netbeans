e48eac27a3a49f145bbe8efd1f3e188a23bd5066
Alfonsinfo <itsmealfons@gmail.com>
2022-07-23 22:32:04 +0700

    brainstorming awal

--- 3 Aplikasi Apotek/JavaApplication12/src/model/Obat.java
+++ 3 Aplikasi Apotek/JavaApplication12/src/model/Obat.java
@@ -5,5 +5,17 @@ package model;
  * @author Alfonsus Setiawan Jacub
  */
 public class Obat {
+    private String kodeObat;
+    private String namaObat;
+    private String sediaan; // Botol Syrup, Tablet , Kapsul
+    private String satuan; // dus / tablet / 1 kapsul 1 dus berapa obat
+    private float hargaJual;  // dosis obat gimana?
     
 }
+// Coba buat fungsi pembelian obat biasa terlebih dahulu
+// Kaitkan dengan user ada 2 versi : member, tidak member, jika member diskon, jika tidak member , tidak diskon
+// fungsi pegawai : menambah, mengurangi, menghapus stok. 
+
+//Wajar tidak tanpa akun bisa beli obat? tidak kan 
+
+// atau kasir bisa melayani pembelian, secara offline. ada sistemnya, dibuat harganya berbeda dengan yang di login akun 
