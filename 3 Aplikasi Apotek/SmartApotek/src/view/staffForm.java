8026a9ec0f9d21b8782a956fdae5b8c9a2cb6dc0
Alfonsinfo <itsmealfons@gmail.com>
2022-07-25 15:11:47 +0700

    Sebelum merubah struktur dari model

--- 3 Aplikasi Apotek/SmartApotek/src/view/staffForm.java
+++ 3 Aplikasi Apotek/SmartApotek/src/view/staffForm.java
@@ -4,13 +4,14 @@ package view;
  *
  * @author Alfonsus Setiawan Jacub
  */
-public class Template extends javax.swing.JFrame {
+public class staffForm extends javax.swing.JFrame {
 
     /**
      * Creates new form Choice
      */
-    public Template() {
+    public staffForm() {
         initComponents();
+        setResizable(false);
     }
 
     /**
@@ -110,21 +111,23 @@ public class Template extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Template.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(staffForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Template.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(staffForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Template.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(staffForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Template.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(staffForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+        //</editor-fold>
         //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Template().setVisible(true);
+                new staffForm().setVisible(true);
             }
         });
     }
