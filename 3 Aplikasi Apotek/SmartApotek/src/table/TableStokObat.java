ac01b5bb0aa68bea95dbe5e99b5ac27be0d45220
Alfonsinfo <itsmealfons@gmail.com>
2022-07-27 20:13:15 +0700

    Mulai mengolah data obat, Karyawan belum siap, tp skip dlu aja

---
+++ 3 Aplikasi Apotek/SmartApotek/src/table/TableStokObat.java
@@ -0,0 +1,13 @@
+package table;
+
+import java.util.List;
+import javax.swing.table.AbstractTableModel;
+import model.Obat;
+public class TableStokObat {
+    private List<Obat> list;
+    
+    public TableStokObat()
+    {
+        this.list = list;
+    }
+}
