f527cc4c4305a6f303a6497752feaf3119ac54d6
Alfonsinfo <itsmealfons@gmail.com>
2022-07-28 07:46:16 +0700

    Olah Data Obat Kelar

--- 3 Aplikasi Apotek/SmartApotek/src/model/Obat.java
+++ 3 Aplikasi Apotek/SmartApotek/src/model/Obat.java
@@ -1,16 +1,113 @@
 package model;
 
-/**
- *
- * @author Alfonsus Setiawan Jacub
- */
 public class Obat {
-    private String kodeObat; // prioritas
-    private String namaObat; // prioritas
-    private String sediaan; // Botol Syrup, Tablet , Kapsul //prioritas
-    //private String satuan; // dus / tablet / 1 kapsul 1 dus berapa obat kayaknya ribet
-    private float hargaJual;  // dosis obat gimana? // prioritas  stok bagaimana ?
-    private int Stok;
+    //Field
+    private String kodeObat;
+    private String namaObat;
+    private String sediaan;
+    private String khasiat;
+    private float hargaJual;
+    private float modal;
+    private int stok;
+
+    
+    //Default Konstruktor
+    public Obat() {
+    }
+    
+    //Konstruktor jika ingin semua inputan dari user
+    public Obat(String kodeObat, String namaObat, String sediaan, String khasiat, float hargaJual, float modal,int stok) {
+        this.kodeObat = kodeObat;
+        this.namaObat = namaObat;
+        this.sediaan = sediaan;
+        this.khasiat = khasiat;
+        this.hargaJual = hargaJual;
+        this.modal = modal;
+        this.stok = stok;
+    }
+    //Konstruktor
+    public Obat(String kodeObat, String namaObat, String sediaan, String khasiat, float modal,int stok) {
+        this.kodeObat = kodeObat;
+        this.namaObat = namaObat;
+        this.sediaan = sediaan;
+        this.khasiat = khasiat;
+        this.modal = modal;
+        this.hargaJual = (120.f/100.f)*modal; // asumsi : keuntungan obat diambil 20% harga modal // menggunakan .f agar float
+        this.stok = stok;
+        System.out.println("Konstruktor 3");
+    }
+    
+    //Method
+
+    //Kode Obat
+    public String getKodeObat() {
+        return kodeObat;
+    }
+
+    public void setKodeObat(String kodeObat) {
+        this.kodeObat = kodeObat;
+    }
+
+    //Nama Obat
+    public String getNamaObat() {
+        return namaObat;
+    }
+
+    public void setNamaObat(String namaObat) {
+        this.namaObat = namaObat;
+    }
+    
+    //Sediaan
+    public String getSediaan() {
+        return sediaan;
+    }
+
+    public void setSediaan(String sediaan) {
+        this.sediaan = sediaan;
+    }
+    
+    //Harga Jual
+    public float getHargaJual() {
+        return hargaJual;
+    }
+
+    public void setHargaJual(float hargaJual) {
+        this.hargaJual = hargaJual;
+    }
+
+    //Khasiat
+    public String getKhasiat() {
+        return khasiat;
+    }
+
+    public void setKhasiat(String khasiat) {
+        this.khasiat = khasiat;
+    }
+
+    //Modal
+    public float getModal() {
+        return modal;
+    }
+
+    public void setModal(float modal) {
+        this.modal = modal;
+    }
+
+    public int getStok() {
+        return stok;
+    }
+
+    public void setStok(int stok) {
+        this.stok = stok;
+    }
+    
+   
+    
+    
+    
+    
+    
+    
 }
 // Coba buat fungsi pembelian obat biasa terlebih dahulu
 // Kaitkan dengan user ada 2 versi : member, tidak member, jika member diskon, jika tidak member , tidak diskon
